55d082254dc24ff8235da5857b4bc38b9a5cb567
Alex Damsa <alexdamsa25092001@gmail.com>
2020-10-06 11:11:01 +0300

    Added dialog messages

--- ASC1/Program.cs
+++ ASC1/Program.cs
@@ -7,16 +7,43 @@ namespace ASC1
         static void Main(string[] args)
         {
             Console.WriteLine("Salut!");
-            Console.WriteLine("Introduceti numarul de pantof: ");
 
-            int numarPantof;
-            try {
-                numarPantof = int.Parse(Console.ReadLine());
-                Console.WriteLine($"Aveti {numarPantof} numarul la pantof");
-            } catch (Exception e) {
-                Console.WriteLine("Va rugam sa folositi doar numere intregi!");
-                return;
-            }
+            string response;
+
+            do
+            {
+                Console.WriteLine("Introduceti numarul de pantof: ");
+
+                int numarPantof;
+                try {
+                    numarPantof = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Aveti {numarPantof} numarul la pantof");
+                } catch (Exception e) {
+                    Console.WriteLine("Va rugam sa folositi doar numere intregi!");
+                    return;
+                }
+
+                if(numarPantof > 44)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Bigfoot", Console.ForegroundColor);
+                }
+                else if(numarPantof > 30)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Normal", Console.ForegroundColor);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Ant", Console.ForegroundColor);
+                }
+
+                Console.WriteLine("Alte numere?");
+                response = Console.ReadLine();
+
+
+            }while(response != "nu");
 
             //int.TryParse(line, out numarPantof);
 
